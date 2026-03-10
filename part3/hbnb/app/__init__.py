91089faf25bd0b119ac4535645da3c272fbebb10
AdeleM-prog <12043@holbertonstudents.com>
2026-03-10 11:06:31 +0100

    update __init__.py to implement Flask config

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from config import DevelopmentConfig
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -8,10 +10,13 @@ from app.api.v1.reviews import api as reviews_ns
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    app.config.from_object(config_class)
+
     db.init_app(app)
+    
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
